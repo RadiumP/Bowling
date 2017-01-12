57c2d9c8205a9a43d16f6c4d5307bdf9f7e80044
RadiumP <chenyan5@asu.edu>
2017-01-12 00:22:33 -0700

    add launch

---
+++ Assets/Scripts/Ball.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ball : MonoBehaviour {
+
+    private AudioSource audioSource;
+    private Rigidbody rb;
+	// Use this for initialization
+	void Start () {
+        rb = GetComponent<Rigidbody>();
+
+        audioSource = GetComponent<AudioSource>();
+
+        Launch();
+        
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    void Launch()
+    {
+        rb.velocity = new Vector3(0, 0, 200f);
+        audioSource.Play();
+    }
+
+}
