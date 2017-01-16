e22e5527223dd8cf92a972956d4fcb2c4f13d8a0
RadiumP <chenyan5@asu.edu>
2017-01-15 21:42:18 -0700

    add swiper and pin detection

---
+++ Assets/Scripts/Pins.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Pins : MonoBehaviour {
+    //no nested prefabs in unity: prefab has multiple gameobjects from another prefab is not linked to it anymore
+
+    public float angleThreshold;
+   
+	// Use this for initialization
+	void Start () {
+       
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        //IsStanding();
+	}
+
+    public bool IsStanding()
+    {
+        Vector3 currentAngle = transform.rotation.eulerAngles;
+        if (Mathf.Abs(currentAngle.x) < angleThreshold && Mathf.Abs(currentAngle.z) < angleThreshold)
+        {
+            return true;
+        }
+        else
+        {
+            Debug.Log("Fall");
+            return false;
+        }
+
+        //return true;
+    }
+}
